2fc01a10833651c7bd4425d42c2897a38eee7b3f
Abebaye <skylala@macbook-air-857.wireless.dyn.umd.edu>
2025-12-15 00:28:32 -0500

    updating README

--- api/weather.js
+++ api/weather.js
@@ -2,26 +2,27 @@ const fetch = require("node-fetch");
 
 async function getWeather(city, country) {
   const apiWeatherKey = process.env.WEATHERSTACK_API_KEY;
+  const query = `${city}, ${country}`;
 
   const url =
-    "http://api.weatherstack.com/current" +
+    `http://api.weatherstack.com/current` +
     `?access_key=${apiWeatherKey}` +
-    `&query=${encodeURIComponent(city + ", " + country)}`;
+    `&query=${query}`;
 
   try {
-    const apiResponse = await fetch(url);
-    const weatherData = await apiResponse.json();
+    const response = await fetch(url);
+    const data = await response.json();
 
     if (data.error) {
-      return { error: weatherData.error.info };
+      return { error: data.error.info };
     }
-
     return {
-      temp: data.current.temperature,
-      feels: data.current.feelslike
+      temperature: data.current.temperature,
+      feelsLike: data.current.feelslike
     };
+    
   } catch (err) {
-    return { error: "FAIL: can't fetch weather data" };
+    return { error: "Weatherstack request failed" };
   }
 }
 
