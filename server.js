c1e9c8adeba1cc5f06a14309e9c51192445f4646
Buruk Ayalew <burukayalew@gmail.com>
2025-12-15 23:07:02 -0500

    server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 const app = express();
 
+const getWeather = require('./api/weather');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,33 +23,12 @@ app.get('/', (req, res) => {
 app.post('/trip', async (req, res) => {
     const { name, city, country } = req.body;
 
+    const newTrip = new Trip({ name, city, country });
+    await newTrip.save();
+
     const weather = await getWeather(city);
     res.render('index', { name, city, country, weather });
-});  
-
-async function getWeather(city) {
-    if (!city) return null;
-  
-    const apiKey = process.env.WEATHER_API_KEY;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}&units=metric`;
-  
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-  
-      if (data.cod !== 200) {
-        return { error: data.message };
-      }
-  
-      return {
-        temperature: data.main.temp,
-        feelsLike: data.main.feels_like,
-        description: data.weather[0].description
-      };
-    } catch (err) {
-      return { error: 'Unable to fetch weather data' };
-    }
-  }
+});
 
 const PORT = 3001;
 app.listen(PORT, () => { console.log(`Web server started and running at http://localhost:${PORT}`);});
