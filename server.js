97ebe30938d4311f5a1aa811b9daa0fdbab39d93
Buruk Ayalew <burukayalew@gmail.com>
2025-12-16 00:17:58 -0500

    mongo

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 
 const express = require('express');
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
 
-dotenv.config();
+dotenv.config({path : './.env'});
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -12,6 +12,19 @@ const getWeather = require('./api/weather');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+const tripSchema = new mongoose.Schema({
+  name: String,
+  city: String,
+  country: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+const Trip = mongoose.model('Trip', tripSchema);
 
 
 app.set('view engine', 'ejs');
@@ -26,9 +39,11 @@ app.post('/trip', async (req, res) => {
     const newTrip = new Trip({ name, city, country });
     await newTrip.save();
 
-    const weather = await getWeather(city);
+    const weather = await getWeather(city, country);
     res.render('index', { name, city, country, weather });
 });
 
+
 const PORT = 3001;
 app.listen(PORT, () => { console.log(`Web server started and running at http://localhost:${PORT}`);});
+
